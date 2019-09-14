844ad04120c8b7125c00d92ee4bec0ae7e893644
Pragun Bajracharya <pragunbaj99@gmail.com>
2019-09-14 14:15:08 +0545

    Updates

---
+++ precache-manifest.9d373102929510b4f8f0a61ffdccedd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e025e22af0825cde2dc93badaa9387d",
+    "url": "/react-practise-1/index.html"
+  },
+  {
+    "revision": "7d31f25d387b8ff44593",
+    "url": "/react-practise-1/static/css/main.15ccd4d7.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/react-practise-1/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "7d31f25d387b8ff44593",
+    "url": "/react-practise-1/static/js/main.0067e0b7.chunk.js"
+  },
+  {
+    "revision": "621539f0f2830d21dc86",
+    "url": "/react-practise-1/static/js/runtime~main.f4259491.js"
+  }
+]);
